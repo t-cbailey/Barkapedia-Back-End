2950e25a5e8d8fca8c314307f1079f6552055558
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-06-12 10:38:37 +0100

    basic server back-end

---
+++ app.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+// import parksRouter from "./routers/parksRouter";
+// import reviewsRouter from "./routers/reviewsRouter";
+// import usersRouter from "./routers/usersRouter";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/parks", parksRouter);
+// app.use("/api/reviews", reviewsRouter);
+// app.use("/api/users", usersRouter);
+
+app.get("*", (req: Request, res: Response) =>
+  res.status(404).send({ message: "Endpoint Not Found" })
+);
+
+export default app;
---
+++ listen.ts
@@ -0,0 +1,3 @@
+import app from "./app";
+
+app.listen(9191, () => console.log("Server is listening on port 9191"));
