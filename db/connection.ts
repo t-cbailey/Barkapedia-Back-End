8e7adc0fcb3ab88a73f8c677533ecc49cdc47a1e
t-cbailey <t.cbaileymusic@gmail.com>
2023-06-29 14:22:44 +0100

    updated firebase and auth

--- db/connection.ts
+++ db/connection.ts
@@ -1,14 +1,15 @@
-import admin, { ServiceAccount, firestore } from 'firebase-admin';
-import serviceAccount from '../serviceAccount.json';
+import admin, { ServiceAccount, firestore } from "firebase-admin";
+import serviceAccount from "../serviceAccount.json";
 
 if (process.env.NODE_ENV !== "production") {
-  process.env.FIRESTORE_EMULATOR_HOST = '127.0.0.1:8080';
-  process.env.FIREBASE_AUTH_EMULATOR_HOST = '127.0.0.1:9099';
+  process.env.FIRESTORE_EMULATOR_HOST = "127.0.0.1:8080";
+  process.env.FIREBASE_AUTH_EMULATOR_HOST = "127.0.0.1:9099";
 }
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as ServiceAccount),
-  databaseURL: "https://nc-parks-default-rtdb.europe-west1.firebasedatabase.app"
+  databaseURL:
+    "https://barkapedia-default-rtdb.europe-west1.firebasedatabase.app",
 });
 
 const db: firestore.Firestore = admin.firestore();
